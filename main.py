27885e5872b022ab70c5eb6180169826c31e9182
Jair Vazquez <1722110433@utectulancingo.edu.mx>
2023-10-11 14:35:24 +0000

    commit

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form, File, UploadFile
 import csv
 from pydantic import BaseModel
 import requests 
+
 
 app = FastAPI()
 
@@ -46,6 +47,16 @@ class Contactos(BaseModel):
     s_apellido: str
     email: str
     telefono: int
+
+@app.post("/v1/static", status_code=200,
+    summary="Endpoint para cargar imagen de contacto",
+    description="Endpoint para cargar imagen de contacto")
+def cargar_imagen(id_contacto: int = Form(...), imagen: UploadFile = Form(...)):
+    # Guarda la imagen en un archivo
+    with open(f"imagen/{id_contacto}.jpg", "wb") as image_file:
+        image_file.write(imagen.file.read())
+
+    return {"mensaje": "Imagen cargada por que si"}
 
 @app.post("/v1/contactos", status_code=201,
     summary="Endpoint para enviar datos",
